ef3a33f33f6dd193844bec5b0a4935ee5e8100b4
sumitsrivastava <sumitsrivastava9@gmail.com>
2023-08-04 12:51:41 +0530

    Api codes added

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+
+dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: 'https://techzoid.onrender.com',
+  })
+);
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
+
+app.listen(5000, () => {
+  console.log('Backend is running');
+});
---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+//REGISTER
+router.post('/register', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(400).json('wrong credentials');
+    } else {
+      const validate = await bcrypt.compare(req.body.password, user.password);
+      if (!validate) {
+        res.status(400).json('wrong password');
+      } else {
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+      }
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
